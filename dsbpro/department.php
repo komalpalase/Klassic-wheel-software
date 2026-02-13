04da3e85861e7f1df225c8bbace255e40b93bec9
komalpalase <149053712+komalpalase@users.noreply.github.com>
2026-02-13 10:12:24 +0530

    Delete dsbpro directory

--- dsbpro/department.php
+++
@@ -1,9 +0,0 @@
-<h4>Department Master</h4>
-
-Department ID :
-<input type="text" class="form-control"><br>
-
-Department Name :
-<input type="text" class="form-control"><br>
-
-<button class="btn btn-primary">Save</button>
--- dsbpro/select_company.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-$_SESSION['company'] = $_GET['id'];
-echo "Company Selected";
-?>
